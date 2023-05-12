e6523519740f9817322f8678cb6e5828e7450446
AmirVeiN <amir.aha73566@gmail.com>
2023-05-12 20:01:52 +0330

    calculator finish

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,17 +1,78 @@
-// base
-import * as React from "react";
+import { View, Text, FlatList, TouchableOpacity } from "react-native";
+import { FontAwesome5 } from "@expo/vector-icons";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { useState } from "react";
 
-// types
-import type { NativeStackScreenProps } from "@react-navigation/native-stack";
-import { pagesType } from "./config";
+export default function Home() {
+  const [top, topHandler] = useState("");
 
-// components
-import { View, Text } from "react-native";
+  const addList = (item: string) => topHandler(top + item);
 
-export default function Home(props: NativeStackScreenProps<pagesType, "home">) {
-    return (
-        <View className="w-screen h-screen items-center justify-center">
-            <Text>your app is ready !!!</Text>
-        </View>
-    );
+  const removeLast = () => topHandler(top.substring(0, top.length - 1));
+
+  const [result, resultHandler] = useState("");
+
+  const element = {
+    "A C": (
+      <MaterialCommunityIcons name="alpha-c-circle" size={40} color="white" />
+    ),
+    "B backSpace": <FontAwesome5 name="backspace" size={24} color="white" />,
+    "C %": <MaterialCommunityIcons name="percent" size={24} color="white" />,
+    "D /": <FontAwesome5 name="divide" size={24} color="black" />,
+    "E 7": "7",
+    "F 8": "8",
+    "G 9": "9",
+    "H *": (
+      <MaterialCommunityIcons name="close-thick" size={24} color="black" />
+    ),
+    "I 4": "5",
+    "G 5": "5",
+    "K 6": "6",
+    "L -": <FontAwesome5 name="minus" size={24} color="black" />,
+    "O 1": "1",
+    "M 2": "2",
+    "N 3": "3",
+    "P +": <FontAwesome5 name="plus" size={24} color="black" />,
+    "Q 00": "00",
+    "R 0": "0",
+    "S .": ",",
+    "T res": <FontAwesome5 name="equals" size={24} color="white" />,
+  };
+
+  return (
+    <View className="flex h-full w-full flex-col bg-[#ED6E6E] p-5 pt-6">
+      <View className="flex h-[355] w-full flex-col justify-end space-y-6 pb-3">
+        <Text className="h-1/3 w-full text-right text-3xl text-white/60">
+          {top}
+        </Text>
+        <Text className=" w-full text-right text-5xl  text-white">
+          {result}
+        </Text>
+      </View>
+      <View className="h-[2px] w-full bg-white/25"></View>
+      <View className="h-full w-full">
+        <FlatList
+          numColumns={4}
+          data={Object.entries(element)}
+          renderItem={(e) => (
+            <TouchableOpacity
+              onPress={() => {
+                if (e.item[0].split(" ")[1] === "res")
+                  resultHandler(eval(top)), topHandler(eval(top));
+                else if (e.item[0].split(" ")[1] === "C")
+                  topHandler(""), resultHandler("");
+                else if (e.item[0].split(" ")[1] === "backSpace") removeLast();
+                else addList(e.item[0].split(" ")[1]);
+              }}
+              className={`${
+                e.item[0].split(" ")[1] === "res" && " rounded-full bg-black"
+              } m-[15.5] flex h-16 w-16 items-center  justify-center `}
+            >
+              <Text className={` text-4xl text-white `}>{e.item[1]}</Text>
+            </TouchableOpacity>
+          )}
+        />
+      </View>
+    </View>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,5 @@
-// base
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { pagesType } from "./config";
-
-
-// pages
 import HomePage from "./home";
 
-const { Navigator, Screen, Group } = createNativeStackNavigator<pagesType>();
-
 export default function Pages() {
-    return (
-        <Navigator>
-          <Screen
-            name="home"
-            component={HomePage} 
-          />
-        </Navigator>
-    );
+  return <HomePage />;
 }
