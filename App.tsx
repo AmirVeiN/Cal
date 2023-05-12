7698f78a89bd021d079d8e1cb3733b6cd907d99b
AmirVeiN <amir.aha73566@gmail.com>
2023-05-13 01:28:28 +0330

    version 1.0.2

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import { View, Text, FlatList, TouchableOpacity, Alert } from "react-native";
+import SafeAreaView , {SafeAreaProvider} from "react-native-safe-area-view";
 import { FontAwesome5 } from "@expo/vector-icons";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { useState } from "react";
@@ -60,73 +61,78 @@ export default function App() {
   };
 
   return (
-    <View className="flex h-screen w-screen flex-col bg-[#ED6E6E] p-5 pt-6">
-      <View className="flex h-[355] w-full flex-col justify-end space-y-6 pb-3">
-        <Text className="h-1/3 w-full text-right text-3xl text-white/60">
-          {top}
-        </Text>
-        <Text className=" w-full text-right text-5xl  text-white">
-          {result}
-        </Text>
-      </View>
-      <View className="h-[2px] w-full bg-white/25"></View>
-      <View className="h-full w-full">
-        <FlatList
-          numColumns={4}
-          data={Object.entries(element)}
-          renderItem={(e) => (
-            <TouchableOpacity
-              onPress={() => {
-                if (
-                  (top.slice(-1) === "-" ||
-                    top.slice(-1) === "+" ||
-                    top.slice(-1) === "/" ||
-                    top.slice(-1) === "*" ||
-                    top.slice(-1) === "%") &&
-                  (e.item[0].split(" ")[1] === "+" ||
-                    e.item[0].split(" ")[1] === "-" ||
-                    e.item[0].split(" ")[1] === "/" ||
-                    e.item[0].split(" ")[1] === "%" ||
-                    e.item[0].split(" ")[1] === "*")
-                ) {
-                  createAlert();
-                } else {
-                  if (e.item[0].split(" ")[1] === "res")
-                    resultHandler(eval(top));
-                  else if (e.item[0].split(" ")[1] === "C")
-                    topHandler(""), resultHandler("");
-                  else if (e.item[0].split(" ")[1] === "backSpace")
-                    removeLast();
-                  else {
-                    let count = 0;
-                    for (let i of top) {
-                      if (
-                        i === "+" ||
-                        i === "-" ||
-                        i === "/" ||
-                        i === "*" ||
-                        i === "%"
-                      ) {
-                        count += 1;
+    <SafeAreaProvider>
+      <SafeAreaView forceInset={{ top: "never" }}>
+        <View className="flex h-full w-full flex-col bg-[#ED6E6E] p-5 pt-6">
+          <View className="flex h-[355] w-full flex-col justify-end space-y-6 pb-3">
+            <Text className="h-1/3 w-full text-right text-3xl text-white/60">
+              {top}
+            </Text>
+            <Text className=" w-full text-right text-5xl  text-white">
+              {result}
+            </Text>
+          </View>
+          <View className="h-[2px] w-full bg-white/25"></View>
+          <View className="h-full w-full">
+            <FlatList
+              numColumns={4}
+              data={Object.entries(element)}
+              renderItem={(e) => (
+                <TouchableOpacity
+                  onPress={() => {
+                    if (
+                      (top.slice(-1) === "-" ||
+                        top.slice(-1) === "+" ||
+                        top.slice(-1) === "/" ||
+                        top.slice(-1) === "*" ||
+                        top.slice(-1) === "%") &&
+                      (e.item[0].split(" ")[1] === "+" ||
+                        e.item[0].split(" ")[1] === "-" ||
+                        e.item[0].split(" ")[1] === "/" ||
+                        e.item[0].split(" ")[1] === "%" ||
+                        e.item[0].split(" ")[1] === "*")
+                    ) {
+                      createAlert();
+                    } else {
+                      if (e.item[0].split(" ")[1] === "res")
+                        resultHandler(eval(top));
+                      else if (e.item[0].split(" ")[1] === "C")
+                        topHandler(""), resultHandler("");
+                      else if (e.item[0].split(" ")[1] === "backSpace")
+                        removeLast();
+                      else {
+                        let count = 0;
+                        for (let i of top) {
+                          if (
+                            i === "+" ||
+                            i === "-" ||
+                            i === "/" ||
+                            i === "*" ||
+                            i === "%"
+                          ) {
+                            count += 1;
+                          }
+                        }
+                        if (count <= 20) {
+                          addList(e.item[0].split(" ")[1]);
+                        } else {
+                          MaxAlert();
+                        }
                       }
                     }
-                    if (count <= 20) {
-                      addList(e.item[0].split(" ")[1]);
-                    } else {
-                      MaxAlert();
-                    }
-                  }
-                }
-              }}
-              className={`${
-                e.item[0].split(" ")[1] === "res" && " rounded-full bg-black"
-              } m-[15.5] flex h-16 w-16 items-center  justify-center `}
-            >
-              <Text className={` text-4xl text-white `}>{e.item[1]}</Text>
-            </TouchableOpacity>
-          )}
-        />
-      </View>
-    </View>
+                  }}
+                  className={`${
+                    e.item[0].split(" ")[1] === "res" &&
+                    " rounded-full bg-black"
+                  } m-[15.5] flex h-16 w-16 items-center  justify-center `}
+                >
+                  <Text className={` text-4xl text-white `}>{e.item[1]}</Text>
+                </TouchableOpacity>
+              )}
+            />
+          </View>
+        </View>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
